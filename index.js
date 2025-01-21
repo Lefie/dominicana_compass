ccd821da9a522a4fb96deddabbdde7e2c9a61a55
Lefie <qiwenruan11@gmail.com>
2025-01-21 16:28:14 -0500

    add names of events

--- index.js
+++ index.js
@@ -42,46 +42,7 @@ function createEle(eleName,content){
     return ele
 }
 
-
-async function getData() {
-    const date_obj = new Date()
-    const date = date_obj.getDate()
-    const month = date_obj.getMonth() + 1
-    const year = date_obj.getFullYear()
-    
-    const url = `http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${year}/${month}/${date}`
-    try {
-        const response = await fetch(url, {
-            redirect:"follow"
-        })
-        if (!response.ok){
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        console.log(json)
-        // celebration
-        const celebrations = []
-        if(json && json.celebrations){
-            for( let i = 0; i < json.celebrations.length; i++) {
-                celebrations.push(json.celebrations[i].title)
-            }
-        }
-       
-        
-        const season = document.querySelector("#season")
-        console.log(season)
-        for(let i = 0; i < celebrations.length; i++){
-            const p = createEle("p", celebrations[i])
-            season.appendChild(p)
-        }
-        
-        
-    }catch(err){
-        console.error(err.message);
-    }
-}
-
-async function addDate() {
+function addDate() {
     const date_obj = new Date()
     const date = date_obj.getDate()
     const month = date_obj.getMonth() + 1
@@ -94,6 +55,98 @@ async function addDate() {
     // display 
     const display_date = document.querySelector("#date")
     display_date.innerHTML = formatted_date
+}
+
+function addNames(events) {
+    const names = []
+    for (let i = 0; i < events.length; i++) {
+        names.push(events[i].name)
+    }
+    
+    const season = document.querySelector("#season")
+    console.log(season)
+    for(let i = 0; i < names.length; i++){
+        const p = createEle("p", names[i])
+        season.appendChild(p)
+    }
+}
+
+
+async function getData() {
+
+    const date_obj = new Date()
+    const day = date_obj.getDate()
+    const month = date_obj.getMonth() + 1
+    const year = date_obj.getFullYear()
+    const nextYear = date_obj.getFullYear() + 1
+    
+    const url = `https://litcal.johnromanodorazio.com/api/dev/calendar/${year}`
+    const url2 = `https://litcal.johnromanodorazio.com/api/dev/calendar/${nextYear}`
+
+    const option = {
+        "method": "GET",
+        "headers": {
+          "accept": "application/json",
+          "Accept-Language": "en_US"
+        }
+    };
+    
+    try {
+        const response = await fetch(url, option)
+        const response_ny = await fetch(url2, option)
+        
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        if (!response_ny.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        const json2 = await response_ny.json();
+
+        const litcal = json.litcal
+        const litcal_ny = json2.litcal
+        const eventsOfYr = getEvents(litcal, litcal_ny, year)
+        console.log(eventsOfYr)
+        const events = queryEvents(eventsOfYr, month, day)
+        // names of events 
+        addNames(events)
+        
+    }catch(err){
+        console.error(err.message);
+    }
+}
+
+function getEvents(litcal, litcal2, year) {
+    events_of_the_year = []
+    for(let i = 0; i < litcal.length; i++){
+        if(litcal[i].year === year) {
+            events_of_the_year.push(litcal[i])
+        }
+    }
+
+    for(let i = 0; i < litcal2.length; i++){
+        if(litcal2[i].year === year) {
+            events_of_the_year.push(litcal2[i])
+        }
+    }
+
+    return events_of_the_year
+}
+
+function queryEvents(litcal, month, day) {
+   
+    events = []
+    for ( let i = 0; i < litcal.length; i++) {
+        if(litcal[i].month === month && litcal[i].day === day ) {
+            console.log(litcal[i])
+            events.push(litcal[i])
+        }
+    }
+ 
+    return events
+
 }
 
 addDate()
